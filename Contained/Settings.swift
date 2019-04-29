d4ada98e0ac02b64817351a49bbd8b019a38999c
alexladines <alexladines@outlook.com>
2019-04-29 13:25:38 -0700

    Built model and organized files.

---
+++ Contained/Settings.swift
@@ -0,0 +1,14 @@
+//
+//  Settings.swift
+//  Contained
+//
+//  Created by Alex Ladines on 4/29/19.
+//  Copyright © 2019 Alex Ladines. All rights reserved.
+//
+
+import Foundation
+
+class Settings {
+    var shouldRoll = false
+    var shouldZoon = false
+}
